028bb43dc2f6ca157df769fb2fd7316c7cf66dde
Alexander-Wen <alex@alexanderwen.me>
2016-03-29 19:37:47 -0400

    add some basic test data, and basic functionality

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+'use strict';
+
+const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'ninteen'];
+const tens = ['ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
 function standardizeTime(time) {
-  return time;
+  let result = time;
+  ones.map((number, index) => result = result.replace(number, index));
+  teens.map((number, index) => result = result.replace(number, index + 10));
+  tens.map((number, index) => result = result.replace(number, (index + 1) * 10));
+
+  // result = result
+  //   .replace(/-/g, ' ');
+
+  return result;
 }
 
 module.exports = standardizeTime;
